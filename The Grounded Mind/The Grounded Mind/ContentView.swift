d92d1c0e61695248b8d7859e75ac32d78bd29d24
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-06-23 13:52:03 +0100

    Created iOS Project

---
+++ The Grounded Mind/The Grounded Mind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Grounded Mind
+//
+//  Created by Abdul Moiz on 23/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Grounded Mind/The Grounded Mind/The_Grounded_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Grounded_MindApp.swift
+//  The Grounded Mind
+//
+//  Created by Abdul Moiz on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct The_Grounded_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
